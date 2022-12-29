d8ea2efc763aecdd12dd87796cdc67ce6c28c22f
Vedasto <prajesh.eleven118@gmail.com>
2022-12-29 20:47:50 +0530

    created strings file

---
+++ strings/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Let's learn about Character Arrays and Strings in C++" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+
+    string s = "Hello World";
+
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "by @eleven" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+
+    return 0;
+}
