73c0c91f6108a042333b3a5c4331c16ec68c2848
Vedasto <prajesh.eleven118@gmail.com>
2022-12-29 20:18:37 +0530

    added few functions

--- algos/main.cpp
+++ algos/main.cpp
@@ -17,7 +17,41 @@ int main()
     // returns true if element is present in the array
     cout << "[*] Binary Search:\n[*] Searching if 3 is present or not: " << binary_search(v.begin(), v.end(), 3) << endl;
 
-    cout << "-----------------------------------------------" << endl;
+    // max(a, b);
+    cout << "[*] Max of 2, 3 is: " << max(2, 3) << endl;
+    // min(a, b);
+    cout << "[*] Min of 2, 3 is: " << min(2, 3) << endl;
+    // swap(a, b);
+
+    // string operations
+    string s = "bd";
+    cout << "[*] s: " << s << endl;
+    reverse(s.begin(), s.end());
+    cout << "[*] Reversed s: " << s << endl;
+
+    // rotate vector
+    cout << "[*] Vector before rotation: " << endl;
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << "\n[*] Vector after rotation: " << endl;
+    rotate(v.begin(), v.begin() + 2, v.end());
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    // sorting
+    // known as Intro Sort
+    // is a combination of 3 sorts - quick, heap, insertion
+    sort(v.begin(), v.end());
+    cout << "\n[*] Intro Sort: " << endl;
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+
+    cout << "\n-----------------------------------------------" << endl;
     cout << "by @eleven" << endl;
     cout << "-----------------------------------------------" << endl;
     return 0;
